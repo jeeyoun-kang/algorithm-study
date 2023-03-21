2690b76aa8f31f0ca4d2680f61a1f17e22cc40c2
JYKANG <xadamantiumx@naver.com>
2023-03-21 22:08:31 +0900

    12주차

--- 강지윤/12주차/2805_나무자르기.py
+++ 강지윤/12주차/2805_나무자르기.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin .readline
-n,m = map(int, input().split())
+n,m = map(int, input().split()) #나무의 수와 가져가려는 나무의 길이
 
 trees = list(map(int, input().split()))
 
@@ -14,7 +14,7 @@ while left <= right:
         if tree >= mid:
             total += tree - mid
 
-    if total >= m:
+    if total >= m: #가져가려는 나무가 m보다 클때
         left = mid + 1
     else:
         right = mid - 1
